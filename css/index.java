72bea9aedeb755f0077339d27e943b98772b33d1
shahzodkuchkarov <shahzodk0@gmail.com>
2021-09-02 11:34:29 +0400

    modified

---
+++ css/index.java
@@ -0,0 +1,9 @@
+public class A{
+	public statis void main (String[] args) {
+        int k;
+		k = 7;
+		System.out.println("\nEntered data:\n\nint   =" + 
+		       k + "\nString = " + s + "\ndouble =" +
+			   x + "\ndouble2= " + y + "\m");
+	}
+}
